7afea0fcdb2ed4b2b951f0fff7a05c62a76206eb
Zach Boatrite <zach@tenforwardconsulting.com>
2016-02-21 16:26:37 -0600

    Add methods to get and set money

--- lib/pse/save.rb
+++ lib/pse/save.rb
@@ -14,21 +14,21 @@ class PSE::Save
   end
 
   def gender
-    IO.binread path, PSE::Datum::GENDER.length, PSE::Datum::GENDER.offset
+    read PSE::Datum::GENDER.length, PSE::Datum::GENDER.offset
   end
 
   def gender=(gender)
     unless GENDERS.include? gender
       raise ArgumentError.new "Invalid gender. Given #{gender.to_hex_s} but must be one of #{GENDERS.map { |g| g.to_hex_s }}"
     end
-    IO.binwrite path, gender, PSE::Datum::GENDER.offset
+    write gender, PSE::Datum::GENDER.offset
   end
 
   def player_name
-    name_padding_bytes = [0x50, 0x00]
-    byte_string = IO.binread path, PSE::Datum::PLAYER_NAME.length, PSE::Datum::PLAYER_NAME.offset
+    name_padding_bytes = [0x50]
+    byte_string = read PSE::Datum::PLAYER_NAME.length, PSE::Datum::PLAYER_NAME.offset
     byte_string
-      .bytes
+      .bytes[0..6]
       .reject { |byte| name_padding_bytes.include? byte }
       .map { |byte| PSE::HEX_TO_CHARS.fetch byte }
       .join
@@ -56,12 +56,42 @@ class PSE::Save
     bytes += padding
     # Each array element is a signed 8-bit, so we use 'c' to pack them.
     byte_string = bytes.pack('c*')
-    IO.binwrite path, byte_string, PSE::Datum::PLAYER_NAME.offset
+    write byte_string, PSE::Datum::PLAYER_NAME.offset
+    update_checksum
+  end
+
+  def rival_name
+    name_padding_bytes = [0x50]
+    byte_string = read PSE::Datum::RIVAL_NAME.length, PSE::Datum::RIVAL_NAME.offset
+    byte_string
+      .bytes[0..6]
+      .reject { |byte| name_padding_bytes.include? byte }
+      .map { |byte| PSE::HEX_TO_CHARS.fetch byte }
+      .join
+  end
+
+  def money
+    byte_string = read PSE::Datum::MONEY.length, PSE::Datum::MONEY.offset
+    bytes = byte_string.bytes
+    # The first byte is the number of multiples of 2^16 (65536)
+    # The second byte is the number of multiples of 2^8 (256)
+    # The third byte is the number of multiples of 2^0 (1)
+    [bytes[0] * 2**16, bytes[1] * 2**8, bytes[2]].reduce(:+)
+  end
+
+  def money=(new_money)
+    if new_money < 0 || new_money > 999999
+      raise ArgumentError.new "Money must be between 0 and 999,999"
+    end
+    first_byte, remainder = new_money.divmod 2**16
+    second_byte, third_byte = remainder.divmod 2**8
+    byte_string = [first_byte, second_byte, third_byte].pack 'c*'
+    write byte_string, PSE::Datum::MONEY.offset
     update_checksum
   end
 
   def current_checksum
-    IO.binread path, PSE::Datum::CHECKSUM.length, PSE::Datum::CHECKSUM.offset
+    read PSE::Datum::CHECKSUM.length, PSE::Datum::CHECKSUM.offset
   end
 
   def calculate_checksum
@@ -75,6 +105,20 @@ class PSE::Save
 
   def update_checksum
     # This is sufficient for the game to use the modified data.
-    IO.binwrite path, calculate_checksum, PSE::Datum::CHECKSUM.offset
+    write calculate_checksum, PSE::Datum::CHECKSUM.offset
+  end
+
+  def test
+    require 'pry'; binding.pry
+  end
+
+  private
+
+  def read(length, offset)
+    IO.binread path, length, offset
+  end
+
+  def write(byte_string, offset)
+    IO.binwrite path, byte_string, offset
   end
 end
